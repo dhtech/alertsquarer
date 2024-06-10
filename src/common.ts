a516632e5fc303899d185beb3e4328ca51494b48
The Louie <github@louie.se>
2024-06-10 18:35:44 +0200

    minor fixes

--- src/common.ts
+++ src/common.ts
@@ -1,1 +1,14 @@
 export const wait = async (ms: number): Promise<void> => await new Promise((resolve: () => void) => setTimeout(resolve, ms))
+
+export const setupLog = () => {
+    const originalLog = console.log
+    // Overwriting
+    console.log = function () {
+      var args = [].slice.call(arguments);
+      originalLog.apply(console.log,[getCurrentDateString()].concat(args));
+    };
+    // Returns current timestamp
+    function getCurrentDateString() {
+      return (new Date()).toISOString() + ' ::';
+    };
+  }
