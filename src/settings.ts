d6964d28784a4431666413c2aef561d4db036069
The Louie <github@louie.se>
2024-06-12 15:32:30 +0200

    production ready summer 2024

--- src/settings.ts
+++ src/settings.ts
@@ -3,11 +3,10 @@ import type { Team } from './types'
 export const host = '127.0.0.1'
 export const port = 8080
 
-export const teams: Team[] = ['core', 'access', 'services']
-export const defaultTTL = 605000
-//export const defaultTTL = 120000
-export const heartbeatTTL = 10000
-export const updateMs = 200
+export const teams: Team[] = ['access', 'core', 'services', 'observer']
+export const defaultTTL = 60000    // Milliseconds
+export const heartbeatTTL = 1500   // Milliseconds
+export const updateMs = 200        // Milliseconds
 
 export const debugOutputInterval = 2000
 export const pruneInterval = 5000
