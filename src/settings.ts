5556b65b6b415e6ae4ac48a5a6f7edcbb108719c
The Louie <github@louie.se>
2024-06-13 19:25:35 +0200

    improve display of missing heartbeat

--- src/settings.ts
+++ src/settings.ts
@@ -3,9 +3,9 @@ import type { Team } from './types'
 export const host = '127.0.0.1'
 export const port = 8080
 
-export const teams: Team[] = ['access', 'core', 'services', 'observer']
+export const teams: Team[] = ['services', 'access', 'core', 'observer']
 export const defaultTTL = 60000    // Milliseconds
-export const heartbeatTTL = 1500   // Milliseconds
+export const heartbeatTTL = 10000   // Milliseconds
 export const updateMs = 200        // Milliseconds
 
 export const debugOutputInterval = 2000
